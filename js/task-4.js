4cc95b558a9315622839410261079b914effe141
Oleh Musii <oleg.musiy321@gmail.com>
2026-01-21 21:23:38 +0200

    Update tasks

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,24 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    const form = event.currentTarget;
+    const emailInput = form.elements.email;
+    const emailValue = emailInput.value.trim();   
+    const passwordInput = form.elements.password;  
+    const passwordValue = passwordInput.value.trim();
+    
+if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+} 
+    
+const obj = {
+    email: emailValue,
+    password: passwordValue
+    }; 
+    
+    console.log(obj);
+    form.reset();
+});
 
